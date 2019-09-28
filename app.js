0a0863e54d545602cf69cef71fa5a4c3af1046f0
Angel <aoanderson@sdsu.edu>
2019-09-27 17:34:36 -0700

    Assignment Complete

---
+++ app.js
@@ -0,0 +1,49 @@
+function onReady() {
+  const ADD_TODO_FORM = document.getElementById('addToDoForm');
+  const NEW_TODO_TEXT = document.getElementById('newToDoText');
+  const TODO_LIST = document.getElementById('toDoList');
+
+  ADD_TODO_FORM.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // get the text
+    let title = NEW_TODO_TEXT.value;
+
+    // create a new li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+
+    //Delete Button
+    let deleteBtn = document.createElement('buttin');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click' , function(event){
+
+      //Remove Li parent
+      TODO_LIST.removeChild(this.parentElement);
+    })
+
+    // set the title
+    newLi.textContent = title;
+
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+    newLi.appendChild(deleteBtn);
+
+    // attach the li to the ul
+    TODO_LIST.appendChild(newLi);
+
+    //empty the input
+    NEW_TODO_TEXT.value = '';
+  });
+};
+
+window.onload = function() {
+   onReady();
+ };
